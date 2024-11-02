91434559c1ddf00a427b8c6f08faddc49a1f7cb3
lukslawe <lukslawe@gmail.com>
2024-11-03 00:02:31 +0100

    Skali wartosci na osi Y i formatowanie czasu na osi X

--- app/src/main/java/com/example/telemedycynaapp/ChartActivity.kt
+++ app/src/main/java/com/example/telemedycynaapp/ChartActivity.kt
@@ -15,7 +15,9 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+
 
 
 class ChartActivity : ComponentActivity() {
@@ -43,12 +45,28 @@ class ChartActivity : ComponentActivity() {
         lineChart.setTouchEnabled(true)
         lineChart.description.isEnabled = false
         lineChart.setPinchZoom(true)
+        lineChart.axisLeft.axisMaximum=60f;
+        lineChart.axisLeft.textColor=Color.WHITE
+        lineChart.axisLeft.axisLineColor=Color.WHITE
+        lineChart.axisLeft.textSize=16f
+        lineChart.axisLeft.axisMinimum=0f;
+        lineChart.axisLeft.granularity=10f; // Ustawienie odstępu co 10 jednostek
+        lineChart.axisLeft.isGranularityEnabled = true;
+        lineChart.xAxis.valueFormatter = TimeValueFormatter();
+        lineChart.xAxis.textColor=Color.WHITE
+        lineChart.xAxis.axisLineColor=Color.WHITE
+        lineChart.xAxis.textSize=16f
+        lineChart.xAxis.position=XAxis.XAxisPosition.BOTTOM;
+        lineChart.axisRight.isEnabled=false;
+        lineChart.legend.textColor=Color.WHITE
+        lineChart.legend.textSize=18f
+
         val marker=Marker(this)
         marker.chartView=lineChart
         lineChart.marker=marker
 
         val description = Description().apply {
-            text = "Czas (sekundy)"
+            text = "Czas"
             textSize = 16f
             textColor = Color.WHITE
             //setPosition(2f, - 2f)  // pozycja w prawym dolnym rogu
@@ -72,6 +90,10 @@ class ChartActivity : ComponentActivity() {
     }
 
     private fun processData(data: Float) {
+        if (data > 55f) {
+            lineChart.axisLeft.axisMaximum = 100f;
+            lineChart.axisLeft.labelCount=10;
+        }
         entries.add(Entry(dataCount.toFloat(), data))
         dataCount+=2
         dataSet = LineDataSet(entries, "Wilgotność")
@@ -87,23 +109,26 @@ class ChartActivity : ComponentActivity() {
         dataSet.valueTextSize = 15f
         dataSet.color = Color.YELLOW
         dataSet.lineWidth=3f
-        lineChart.axisLeft.textColor=Color.WHITE
-        lineChart.axisLeft.axisLineColor=Color.WHITE
-        lineChart.axisLeft.textSize=16f
-        lineChart.axisRight.textColor=Color.WHITE
-        lineChart.axisRight.axisLineColor=Color.WHITE
-        lineChart.axisRight.textSize=16f
-        lineChart.xAxis.textColor=Color.WHITE
-        lineChart.xAxis.axisLineColor=Color.WHITE
-        lineChart.xAxis.textSize=16f
-        lineChart.xAxis.position=XAxis.XAxisPosition.BOTTOM;
-        lineChart.legend.textColor=Color.WHITE
-        lineChart.legend.textSize=18f
+
     }
 
     private fun refreshChart() {
         val lineData = LineData(dataSet)
         lineChart.data = lineData
         lineChart.invalidate()
+    }
+}
+class TimeValueFormatter : ValueFormatter() {
+    override fun getFormattedValue(value: Float): String {
+        val totalSeconds = value.toInt()
+        val hours = totalSeconds / 3600
+        val minutes = (totalSeconds % 3600) / 60
+        val seconds = totalSeconds % 60
+
+        return if (hours > 0) {
+            String.format("%d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format("%d:%02d", minutes, seconds)
+        }
     }
 }
