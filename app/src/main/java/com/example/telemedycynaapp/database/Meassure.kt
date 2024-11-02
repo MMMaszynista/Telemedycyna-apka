5e05a477aeb1275f6d8caae1d0004d7e1fa9d688
MMaszynista <djdamian20011@gmail.com>
2024-11-02 13:28:17 +0100

    Dodane zaleznosci do bazy i utworzone klasy do zbierania danych

---
+++ app/src/main/java/com/example/telemedycynaapp/database/Meassure.kt
@@ -0,0 +1,12 @@
+package com.example.telemedycynaapp.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "meassures")
+data class Meassure(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val date: String,
+    val humidity: Float
+)
